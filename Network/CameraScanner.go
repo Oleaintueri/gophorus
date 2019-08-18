26d8851bb297025f468858505373549523612ec0
Alano <alanopi.314@gmail.com>
2019-08-19 01:36:17 +0200

    semaphore implementation

--- Network/CameraScanner.go
+++ Network/CameraScanner.go
@@ -23,7 +23,8 @@ type Camera struct {
 }
 
 func Ulimit() int64 {
-	out, err := exec.Command("ulimit", "-n").Output()
+	out, err := exec.Command("/bin/sh", "-c", "ulimit -n").Output()
+
 	if err != nil {
 		panic(err)
 	}
@@ -42,50 +43,70 @@ func ScanPort(ip string, port int, timeout time.Duration) (isOpen bool, err erro
 
 	if err != nil {
 		if strings.Contains(err.Error(), "too many open files") {
+			println("too many files open..")
 			time.Sleep(timeout)
 			return ScanPort(ip, port, timeout)
 		}
-	}
-	err = conn.Close()
-	if err != nil {
 		return false, err
+
 	}
-	return true, nil
+	if conn != nil {
+		err = conn.Close()
+		if err != nil {
+			return false, err
+		}
+		return true, nil
+	}
+	return false, nil
 }
 
-func (ps *Semaphore) RunHelper(camera []Camera) (openCameras []string) {
+func (semaphore *Semaphore) RunHelper(camera []Camera) (openCameras []string) {
 	wg := sync.WaitGroup{}
 	defer wg.Wait()
 	outputChannel := make(chan Camera)
 	for i := range camera {
 		wg.Add(1)
-		ps.lock.Acquire(context.TODO(), 1)
-		tmpCamera := camera[i]
-		go func(tmpCamera Camera) {
-			defer ps.lock.Release(1)
+		err := semaphore.lock.Acquire(context.Background(), 1)
+		if err != nil {
+			panic(err)
+		}
+		tmpCamera := &camera[i]
+		go func() {
+			defer semaphore.lock.Release(1)
 			defer wg.Done()
+			fmt.Println("Testing port " + strconv.Itoa(tmpCamera.port) + " with IP " + tmpCamera.ip)
 			isOpen, err := ScanPort(tmpCamera.ip, tmpCamera.port, tmpCamera.timeout)
 			if err == nil {
 				if isOpen == true {
-					outputChannel <- tmpCamera
+					outputChannel <- *tmpCamera
+				} else {
+					fmt.Println("Port of IP " + tmpCamera.ip + " closed")
 				}
+			} else {
+				println(err.Error())
 			}
-		}(tmpCamera)
+		}()
 	}
+	count := 0
 	for {
 		val, ok := <-outputChannel
-		if ok == false {
+		if !ok {
 			break
 		}
+		fmt.Println("Port of IP "+val.ip+" open\n", "Calls: "+strconv.Itoa(count))
 		openCameras = append(openCameras, val.ip)
+		count++
 	}
-	return
+	println("Ended ScanHelper")
+	return openCameras
 }
 
 func Run(ipRange string, port int) []string {
 	cameras := parseIpRange(ipRange, port)
 	s := &Semaphore{lock: semaphore.NewWeighted(Ulimit()),}
-	return s.RunHelper(cameras)
+	opens := s.RunHelper(cameras)
+	fmt.Println("Length of opens: " + strconv.Itoa(len(opens)))
+	return opens
 }
 
 func parseIpRange(ipRange string, port int) (cameraScanner []Camera) {
@@ -97,7 +118,6 @@ func parseIpRange(ipRange string, port int) (cameraScanner []Camera) {
 	endIPArr := strings.Split(end, ".")
 
 	startInt, err := strconv.ParseInt(startIPArr[len(startIPArr)-1:][0], 10, 32)
-
 	if err != nil {
 		panic(err)
 	}
@@ -106,10 +126,10 @@ func parseIpRange(ipRange string, port int) (cameraScanner []Camera) {
 		panic(err)
 	}
 
-	baseIP := strings.Join(startIPArr[:len(startIPArr)-1], "")
-	for i := startInt; i < endInt; i++ {
+	baseIP := strings.Join(startIPArr[:len(startIPArr)-1], ".")
+	for i := startInt; i <= endInt; i++ {
 		cameraScanner = append(cameraScanner, Camera{
-			ip:      baseIP + string(i),
+			ip:      baseIP + "." + strconv.FormatInt(int64(i), 10),
 			port:    port,
 			timeout: 3000 * time.Millisecond,
 		})
--- Network/CameraScanner_test.go
+++ Network/CameraScanner_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 func TestRunt(t *testing.T) {
-	got := Run("41.188.226.1-41.188.226.250", 1935)
+	got := Run("185.60.219.1-41.185.60.219.4", 80)
 	fmt.Println(got)
 }
 
 func BenchmarkRun(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Run("41.188.226.1-41.188.226.250", 1935)
+		Run("185.60.219.1-41.185.60.219.250", 80)
 	}
 }
